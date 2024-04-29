f98cbf23bc362d547919ef856c79229e3ae3d96c
Yasinj05 <yasinj7658@gmail.com>
2024-04-29 09:01:24 +0330

    Updating the Book model

--- models/Book.mjs
+++ models/Book.mjs
@@ -2,29 +2,60 @@ import mongoose from "mongoose";
 import Joi from "joi";
 
 const bookSchema = new mongoose.Schema({
-  title: { type: String, required: true },
+  title: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 100,
+  },
   author: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: "Author",
+    ref: "Author", // Reference to the Author model
     required: true,
   },
-  publishedDate: Date,
-  genres: [String],
+  genre: {
+    type: String,
+    required: true,
+    enum: [
+      "Fiction",
+      "Non-Fiction",
+      "Science Fiction",
+      "Fantasy",
+      "Mystery",
+      "Other",
+    ],
+  },
+  publicationYear: {
+    type: Number,
+    min: 1800,
+    max: new Date().getFullYear(),
+  },
 });
 
-const Book = mongoose.model("Book", bookSchema);
-
-function validateBook(book) {
+// Validate Book data using Joi
+const validateBook = (book) => {
   const schema = Joi.object({
-    title: Joi.string().required(),
-    author: Joi.string()
-      .regex(/^[0-9a-fA-F]{24}$/)
+    title: Joi.string().min(5).max(100).required(),
+    author: Joi.string().required(),
+    genre: Joi.string()
+      .valid(
+        "Fiction",
+        "Non-Fiction",
+        "Science Fiction",
+        "Fantasy",
+        "Mystery",
+        "Other"
+      )
       .required(),
-    publishedDate: Joi.date().iso(),
-    genres: Joi.array().items(Joi.string()).optional(),
+    publicationYear: Joi.number()
+      .integer()
+      .min(1800)
+      .max(new Date().getFullYear()),
   });
 
   return schema.validate(book);
-}
+};
+
+const Book = mongoose.model("Book", bookSchema);
 
 export { Book, validateBook as validate, bookSchema };
